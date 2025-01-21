a8b7cec02b1c430416218a9a23c800e52a60cab0
juanc <jcmodev12@gmail.com>
2025-01-21 14:49:06 -0500

    ops, crash solved

--- app/src/main/java/com/jkgug/meli_juanc/ui/screen/product/ProductDetailsScreen.kt
+++ app/src/main/java/com/jkgug/meli_juanc/ui/screen/product/ProductDetailsScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
